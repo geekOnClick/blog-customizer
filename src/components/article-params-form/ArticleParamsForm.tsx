2a40390d2c3f978d81693492c12dac69c0106396
geekOnClick <kozloww1508@yandex.ru>
2024-05-01 19:08:52 +0300

    feat: feat: Задание выполнено согласно требованиям

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,123 @@
+import { useState } from 'react';
+import { clsx } from 'clsx';
+
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from '../text';
+import { Select } from '../select';
+import {
+	fontColors as fontColorsOptions,
+	backgroundColors as backgroundColorsOptions,
+	fontFamilyOptions,
+	fontSizeOptions,
+	contentWidthArr,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	applyPageChanges: (pageParams: Record<string, OptionType>) => void;
+};
+
+export const ArticleParamsForm = ({ applyPageChanges }: Props) => {
+	const [isFormOpen, setIsFormOpen] = useState(false);
+	const [isBtnReversed, setIsBtnReversed] = useState(false);
+	const [font, setFont] = useState(fontFamilyOptions[0]);
+	const [fontSize, setFontSize] = useState(fontSizeOptions[0]);
+	const [fontColor, setFontColor] = useState(fontColorsOptions[0]);
+	const [backgroundColor, setBackgroundColor] = useState(
+		backgroundColorsOptions[0]
+	);
+	const [contentWidth, setContentWidth] = useState(contentWidthArr[0]);
+
+	const reset = () => {
+		setFont(fontFamilyOptions[0]);
+		setFontSize(fontSizeOptions[0]);
+		setFontColor(fontColorsOptions[0]);
+		setBackgroundColor(backgroundColorsOptions[0]);
+		setContentWidth(contentWidthArr[0]);
+	};
+
+	const submit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		applyPageChanges({
+			font,
+			fontSize,
+			fontColor,
+			backgroundColor,
+			contentWidth,
+		});
+		setIsBtnReversed(!isBtnReversed);
+		setIsFormOpen(false);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton
+				handleBtnClick={() => setIsFormOpen(!isFormOpen)}
+				isReversed={isFormOpen}
+			/>
+			<aside
+				className={
+					`${styles.container} ` + `${isFormOpen && styles.container_open}`
+				}>
 				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+					<Text as='h2' size={31} weight={800} uppercase family='open-sans'>
+						Задайте параметры
+					</Text>
+					<div className={styles.space}>
+						<Select
+							options={fontFamilyOptions}
+							selected={font}
+							onChange={(font) => setFont(font)}
+							title='Шрифт'
+						/>
+					</div>
+					<div className={styles.space}>
+						<RadioGroup
+							name='fontSize'
+							options={fontSizeOptions}
+							selected={fontSize}
+							onChange={(size) => setFontSize(size)}
+							title='Размер шрифта'
+						/>
+						<div className={styles.space}>
+							<Select
+								options={fontColorsOptions}
+								selected={fontColor}
+								onChange={(color) => setFontColor(color)}
+								title='Цвет шрифта'
+							/>
+						</div>
+						<div className={styles.space}>
+							<Separator />
+						</div>
+						<div className={styles.space}>
+							<Select
+								options={backgroundColorsOptions}
+								selected={backgroundColor}
+								onChange={(color) => setBackgroundColor(color)}
+								title='Цвет фона'
+							/>
+						</div>
+						<div className={styles.space}>
+							<Select
+								options={contentWidthArr}
+								selected={contentWidth}
+								onChange={(width) => setContentWidth(width)}
+								title='Ширина контента'
+							/>
+						</div>
+					</div>
+					<div className={clsx(styles.bottomContainer, styles.space)}>
+						<Button title='Сбросить' type='reset' onClick={reset} />
+						<Button
+							title='Применить'
+							type='submit'
+							onClick={(e) => submit(e)}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { OptionType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,41 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [font, setFont] = useState(defaultArticleState.fontFamilyOption.value);
+	const [fontSize, setFontSize] = useState(
+		defaultArticleState.fontSizeOption.value
+	);
+	const [fontColor, setFontColor] = useState(
+		defaultArticleState.fontColor.value
+	);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor.value
+	);
+	const [contentWidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth.value
+	);
+
+	const applyPageChanges = (pageParams: Record<string, OptionType>) => {
+		setFont(pageParams.font.value);
+		setFontSize(pageParams.fontSize.value);
+		setFontColor(pageParams.fontColor.value);
+		setBackgroundColor(pageParams.backgroundColor.value);
+		setContentWidth(pageParams.contentWidth.value);
+	};
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': font,
+					'--font-size': fontSize,
+					'--font-color': fontColor,
+					'--container-width': contentWidth,
+					'--bg-color': backgroundColor,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm applyPageChanges={applyPageChanges} />
 			<Article />
 		</div>
 	);
